86b6265f29e47c4f73940c22cbd32a8b6650890f
João Coelho <admin.henyah@gmail.com>
2017-12-18 15:05:01 +0100

    Improving unit test coverage.

--- src/test/java/net/ericsson/emovs/analytics/EMPAnalyticsProviderTest.java
+++ src/test/java/net/ericsson/emovs/analytics/EMPAnalyticsProviderTest.java
@@ -63,6 +63,26 @@ public class EMPAnalyticsProviderTest {
 
         provider.created("s12345", parameters);
         provider.downloadStarted("s12345", null);
+        provider.error("s12345", 0, parameters);
+        provider.started("s12345", 0, parameters);
+        provider.startCasting("s12345", 0, parameters);
+        provider.aborted("s12345", 0, parameters);
+        provider.paused("s12345", 0, parameters);
+        provider.resumed("s12345", 0, parameters);
+        provider.seeked("s12345", 0, parameters);
+        provider.stopCasting("s12345", 0, parameters);
+        provider.handshakeStarted("s12345", parameters);
+        provider.bitrateChanged("s12345", 0, parameters);
+        provider.completed("s12345", parameters);
+        provider.waitingStarted("s12345", 0, parameters);
+        provider.waitingEnded("s12345", 0, parameters);
+        provider.downloadCompleted("s12345", parameters);
+        provider.downloadStopped("s12345", parameters);
+        provider.downloadError("s12345", parameters);
+        provider.downloadPaused("s12345", parameters);
+        provider.downloadResumed("s12345", parameters);
+        provider.ready("s12345", parameters);
+        provider.handshakeStarted("s12345", false, parameters);
 
         Thread.sleep(1000);
 
@@ -71,6 +91,26 @@ public class EMPAnalyticsProviderTest {
         Assert.assertTrue("Playback.Created".equals(provider.payload.getJSONArray("Payload").getJSONObject(0).getString("EventType")));
         Assert.assertTrue("Playback.DeviceInfo".equals(provider.payload.getJSONArray("Payload").getJSONObject(1).getString("EventType")));
         Assert.assertTrue("Playback.DownloadStarted".equals(provider.payload.getJSONArray("Payload").getJSONObject(2).getString("EventType")));
+        Assert.assertTrue("Playback.Error".equals(provider.payload.getJSONArray("Payload").getJSONObject(3).getString("EventType")));
+        Assert.assertTrue("Playback.Started".equals(provider.payload.getJSONArray("Payload").getJSONObject(4).getString("EventType")));
+        Assert.assertTrue("Playback.StartCasting".equals(provider.payload.getJSONArray("Payload").getJSONObject(5).getString("EventType")));
+        Assert.assertTrue("Playback.Aborted".equals(provider.payload.getJSONArray("Payload").getJSONObject(6).getString("EventType")));
+        Assert.assertTrue("Playback.Paused".equals(provider.payload.getJSONArray("Payload").getJSONObject(7).getString("EventType")));
+        Assert.assertTrue("Playback.Resumed".equals(provider.payload.getJSONArray("Payload").getJSONObject(8).getString("EventType")));
+        Assert.assertTrue("Playback.ScrubbedTo".equals(provider.payload.getJSONArray("Payload").getJSONObject(9).getString("EventType")));
+        Assert.assertTrue("Playback.StopCasting".equals(provider.payload.getJSONArray("Payload").getJSONObject(10).getString("EventType")));
+        Assert.assertTrue("Playback.HandshakeStarted".equals(provider.payload.getJSONArray("Payload").getJSONObject(11).getString("EventType")));
+        Assert.assertTrue("Playback.BitrateChanged".equals(provider.payload.getJSONArray("Payload").getJSONObject(12).getString("EventType")));
+        Assert.assertTrue("Playback.Completed".equals(provider.payload.getJSONArray("Payload").getJSONObject(13).getString("EventType")));
+        Assert.assertTrue("Playback.BufferingStarted".equals(provider.payload.getJSONArray("Payload").getJSONObject(14).getString("EventType")));
+        Assert.assertTrue("Playback.BufferingEnded".equals(provider.payload.getJSONArray("Payload").getJSONObject(15).getString("EventType")));
+        Assert.assertTrue("Playback.DownloadCompleted".equals(provider.payload.getJSONArray("Payload").getJSONObject(16).getString("EventType")));
+        Assert.assertTrue("Playback.DownloadStopped".equals(provider.payload.getJSONArray("Payload").getJSONObject(17).getString("EventType")));
+        Assert.assertTrue("Playback.Error".equals(provider.payload.getJSONArray("Payload").getJSONObject(18).getString("EventType")));
+        Assert.assertTrue("Playback.DownloadPaused".equals(provider.payload.getJSONArray("Payload").getJSONObject(19).getString("EventType")));
+        Assert.assertTrue("Playback.DownloadResumed".equals(provider.payload.getJSONArray("Payload").getJSONObject(20).getString("EventType")));
+        Assert.assertTrue("Playback.PlayerReady".equals(provider.payload.getJSONArray("Payload").getJSONObject(21).getString("EventType")));
+        Assert.assertTrue("Playback.HandshakeStarted".equals(provider.payload.getJSONArray("Payload").getJSONObject(22).getString("EventType")));
     }
 
     private class EMPAnalyticsProviderTester extends EMPAnalyticsProvider {
